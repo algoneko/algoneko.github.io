7b8862144088674ac919d435a4ee3a07a9d9d7b5
developerxyzw <44705138+developerxyzw@users.noreply.github.com>
2021-08-02 22:32:18 +0300

    again fix untoggle

--- script/dark.js
+++ script/dark.js
@@ -2,7 +2,7 @@ function is_dark_mode() {
   return document.cookie.indexOf("d=1") !== -1;
 }
 function clear_dark_mode() {
-   document.cookie = document.cookie.replace("d=1","");
+  document.cookie = "d=1; max-age=0";
 }
 function set_dark_mode() {
   document.cookie = "d=1";
